abb18c76e490bd66b4f8bba8e88d5259bc798022
bdmanik97 <inmanik97@gmail.com>
2024-02-03 20:57:44 +0600

    php final

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php num:
+
+
+// $x = 50;
+// $y = 20;
+
+// $z = $x+$y;
+// echo 'The result is='.$z;
+
+
+
+    for($i = 0; $i<=20; $i++){
+        echo $i.'<br>';
+    }
+
+
+?>
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+
+    echo 'hello world';
+    ?>
+</body>
+</html> -->
